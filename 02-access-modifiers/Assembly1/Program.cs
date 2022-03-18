bc77bc7043327d14a2769f1ab984782cc69b783b
Saad Ur Rehman <saad.ur.rehman77@gmail.com>
2022-03-18 23:59:34 +0500

    initial commit

---
+++ 02-access-modifiers/Assembly1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Assembly1
+{
+    public class Assembly1BaseClass
+    {
+        private string privateVariable = "private";
+        protected string protectedVariable = "protected";
+        internal string internalVariable = "internal";
+        internal protected string internalProtectedVariable = "internal + protected";
+        public string publicVariable = "public";
+
+        public void testAccess()
+        {
+            Console.WriteLine(privateVariable);
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(internalProtectedVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+
+    public class Assembly1DerivedClass : Assembly1BaseClass
+    {
+        public void testAccessInDerived()
+        {
+            //Private variable is not accessible in derived class
+            //Console.WriteLine(privateVariable);
+
+            //All of the following are accessible
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(internalProtectedVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+
+    public class Assembly1OtherClass
+    {
+        public void testAccess()
+        {
+            Assembly1BaseClass A = new Assembly1BaseClass();
+
+            //Private and Protected are not available in other classes
+            //Console.WriteLine(A.privateVariable);
+            //Console.WriteLine(A.protectedVariable);
+
+
+            //All of the following are accessible
+            Console.WriteLine(A.internalVariable);
+            Console.WriteLine(A.internalProtectedVariable);
+            Console.WriteLine(A.publicVariable);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly1BaseClass A1 = new Assembly1BaseClass();
+            A1.testAccess();
+
+            Console.WriteLine("\n");
+            
+            Assembly1DerivedClass A2 = new Assembly1DerivedClass();
+            A2.testAccessInDerived();
+
+            Console.WriteLine("\n");
+
+            Assembly1OtherClass A3 = new Assembly1OtherClass();
+            A3.testAccess();
+        }
+    }
+}
---
+++ 02-access-modifiers/Assembly2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Assembly2
+{
+    public class Assembly2DerviedClass : Assembly1.Assembly1BaseClass
+    {
+        public void testAccessInDerived()
+        {
+            //Private and Internal variable is not accessible in derived class
+            //Console.WriteLine(privateVariable);
+            //Console.WriteLine(internalVariable);
+
+            //All of the following are accessible
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalProtectedVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+
+    public class Assembly2OtherClass
+    {
+        public void testAccess()
+        {
+            Assembly1.Assembly1BaseClass A = new Assembly1.Assembly1BaseClass();
+
+            //Console.WriteLine(A.privateVariable);
+            //Console.WriteLine(A.protectedVariable);
+            //Console.WriteLine(A.internalVariable);
+            //Console.WriteLine(A.internalProtectedVariable);
+
+            //Only public are accesible
+            Console.WriteLine(A.publicVariable);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly2DerviedClass A1 = new Assembly2DerviedClass();
+            A1.testAccessInDerived();
+
+            Console.WriteLine("\n");
+
+            Assembly2OtherClass A2 = new Assembly2OtherClass();
+            A2.testAccess();
+        }
+    }
+}
