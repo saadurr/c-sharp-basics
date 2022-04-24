626801024eb261ed33c10097e3fff2ac9f205f95
Saad Ur Rehman <saad.ur.rehman77@gmail.com>
2022-04-25 01:54:21 +0500

    Adds default constructor project

---
+++ 03-constructors/Constructors/Constructors/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * There are four basic types of constructors in C#
+ * Default, Parametrized, Static and Private
+ * 
+ * The default contructor does not take any argument. If its not defined, the compiler provides a default constructor.
+ * 
+ * Parametrized constructor takes an argument. 
+ */
+
+
+using System;
+
+namespace Constructors
+{
+    class Student 
+    { 
+        public string name { get; set; }
+        public int studentID { get; set; }
+        public string collegeName { get; set; }
+
+        //Default Constructor
+        public Student()
+        {
+            name = "default name";
+            studentID = -1;
+            collegeName = "default college";
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+
+            //Default constructor test
+            Console.WriteLine("Default student object values are: ID is {0}, Name is {1}, and college is {2}", s1.studentID, s1.name, s1.collegeName);
+        }
+    }
+}
